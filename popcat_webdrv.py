04d2f558d185ac735c51c4a352a5b976b6f5c06e
GM Huang <61339305+Jamtech77@users.noreply.github.com>
2021-08-18 00:51:59 +0800

    Update popcat_webdrv.py

--- popcat_webdrv.py
+++ popcat_webdrv.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import threading
 
 options=webdriver.ChromeOptions()
 driver = webdriver.Chrome(chrome_options=options)
@@ -7,5 +8,16 @@ url= u"https://popcat.click/"
 driver.get(url)
 popcat = driver.find_element_by_xpath("//*")
 
-while 1:
-    popcat.send_keys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+def press_t1():
+    while 1:
+        popcat.send_keys("TaiwannumberoneTaiwannumberoneTaiwannumberoneTaiwannumberone")
+
+def press_t2():
+    while 1:
+        popcat.send_keys("TaiwannumberoneTaiwannumberoneTaiwannumberoneTaiwannumberone")
+
+t1_thread = threading.Thread(target=press_t1)
+t2_thread = threading.Thread(target=press_t2)
+
+t1_thread.start()
+t2_thread.start()
